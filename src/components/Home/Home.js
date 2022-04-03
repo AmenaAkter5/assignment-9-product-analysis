5d98fd071469a5975dd4fd67ecad4f1ecec7ce2c
unknown <latapata1952@gmail.com>
2022-04-04 03:07:27 +0600

    home component added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,16 +4,21 @@ import './Home.css';
 
 const Home = () => {
     return (
-        <div className="d-flex justify-content-between align-items-center container mx-auto my-5 py-3">
-            <div className="w-50">
-                <h1>Best android app to <br />
-                    learn programming</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas totam optio libero vero non quos eveniet, voluptate suscipit autem et doloremque facilis a maxime adipisci facere. Dolorem labore consequatur in.</p>
+        <section className="container mx-auto">
+            <div className="d-flex justify-content-between align-items-center my-5 py-3">
+                <div className="w-50 pe-3">
+                    <h1 className="mb-4">Best android app to <br />
+                        <span>learn programming</span></h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas totam optio libero vero non quos eveniet, voluptate suscipit autem et doloremque facilis a maxime adipisci facere. Dolorem labore consequatur in.</p>
+                </div>
+                <div>
+                    <img className="rounded rounded-5" src={logo} alt="" />
+                </div>
             </div>
-            <div>
-                <img src={logo} alt="" />
+            <div className="my-4">
+                <h1 className="text-center">Customer Reviews</h1>
             </div>
-        </div>
+        </section>
     );
 };
 
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Reviews = () => {
     return (
         <div>
-            <h1>Review Page</h1>
+            <h1>Customer Reviews</h1>
         </div>
     );
 };
