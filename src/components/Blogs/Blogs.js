0cdd42c0f0f7aa2df80c58e5f8809f7901fb387f
unknown <latapata1952@gmail.com>
2022-04-04 21:47:02 +0600

    blogs component structure setup

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Blogs.css';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1>This is Blog page</h1>
+        <div className="container mx-auto">
+            <div className="blog-container">
+                <article className="article">
+                    <h2>This is Blog page</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque explicabo voluptate ipsum perferendis porro ratione, deleniti rem reprehenderit minus. Voluptates tempore vero libero quia at nisi animi consectetur voluptas labore.</p>
+                </article>
+                <article className="article">
+                    <h2>This is Blog page</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque explicabo voluptate ipsum perferendis porro ratione, deleniti rem reprehenderit minus. Voluptates tempore vero libero quia at nisi animi consectetur voluptas labore.</p>
+                </article>
+            </div>
         </div>
     );
 };
