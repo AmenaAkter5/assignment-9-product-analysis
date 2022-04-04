a25164ae0011d3ed5a5b125adcc7ee74fd69bbe9
unknown <latapata1952@gmail.com>
2022-04-05 00:16:49 +0600

    404 page upadated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import question from '../../images/question.jpg';
 
 const About = () => {
     return (
-        <div>
-            <h1>Know About Us</h1>
+        <div className="container mx-auto my-5">
+            <div className="text-center">
+                <img width={350} src={question} alt="" />
+                <h2 className="mt-5">Wanna know About us?</h2>
+                <h3>Okay, lets make call at 999.</h3>
+            </div>
         </div>
     );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import confused from '../../images/confused.PNG';
+import four from '../../images/four.jpg';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>The page is not Available</h1>
-            <h3>404</h3>
+        <div className="container mx-auto my-5">
+            <h3 className="fw-bold text-center my-5 pt-5">oops! sorry, The page is not Available!</h3>
+            <div className="d-flex justify-content-center mt-5 pt-5">
+                <img width={200} src={four} alt="" />
+                <img width={200} src={confused} alt="" />
+                <img width={200} src={four} alt="" />
+            </div>
         </div>
     );
 };
